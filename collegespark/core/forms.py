9c7c13a846ca8f077ceaf99adc55cf8370eb46dc
Hormoz <hormoz@hoomi.(none)>
2013-04-08 23:52:41 -0700

    some syntax change

--- collegespark/core/forms.py
+++ collegespark/core/forms.py
@@ -1,37 +1,27 @@
 from django import forms
 
+
 class LoginForm(forms.Form):
-    email = forms.EmailField(widget=forms.TextInput(attrs={
-                            'type' : 'email',
-                            'class': 'input-small', 
-                            'placeholder': 'Email',
-                            }
-    ))
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={'type': 'email',
+               'class': 'input-small',
+               'placeholder': 'Email'}))
 
-    password = forms.CharField(widget=forms.PasswordInput(render_value=False,attrs={
-                              'class': 'input-small', 
-                              'placeholder': 'Password',  
-                              }
-    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        render_value=False, attrs={'class': 'input-small',
+                                   'placeholder': 'Password'}))
 
-class SignUpForm(froms.Form):
-    email = forms.EmailField(widget=forms.TextInput(attrs={
-                            'type' : 'email',
-                            'placeholder': 'Your Email',
-                            }
-    ))
 
-    school = forms.CharField(widget=forms.PasswordInput(attrs={
-                            'placeholder': 'Your School Name',  
-                            }
-    ))
+class SignUpForm(forms.Form):
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={'type': 'email',
+               'placeholder': 'Your Email'}))
 
-    password = forms.CharField(widget=forms.PasswordInput(render_value=False,attrs={
-                              'placeholder': 'Password',  
-                              }
-    ))
+    school = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'Your School Name'}))
 
-    re_enter_password = forms.CharField(widget=forms.PasswordInput(render_value=False,attrs={
-                                       'placeholder': 'Re-enter Password',  
-                                       }
-    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        render_value=False, attrs={'placeholder': 'Password'}))
+
+    re_enter_password = forms.CharField(widget=forms.PasswordInput(
+        render_value=False, attrs={'placeholder': 'Re-enter Password'}))
