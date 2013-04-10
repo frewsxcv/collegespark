d326af6723ed1aa9e9b073dd23b6039d0e27ea1c
Corey Farwell <coreyf@rwell.org>
2013-04-09 23:33:50 -0700

    init user

--- collegespark/settings.py
+++ collegespark/settings.py
@@ -24,6 +24,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'core.User'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -122,7 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'collegespark.core',
