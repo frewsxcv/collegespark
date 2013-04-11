a6614ca0b33fab50323a761dd7dd8304e315121e
htarevern <htarevern@yahoo.com>
2013-04-10 18:21:03 -0700

    add user model

--- collegespark/core/admin.py
+++ collegespark/core/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from collegespark.core.models import User
+from collegespark.core.models import User, School, Major
 
 
 class UserAdmin(admin.ModelAdmin):
     pass
 
+class SchoolAdmin(admin.ModelAdmin):
+    pass
+
+class MajorAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(User, UserAdmin)
+admin.site.register(School, SchoolAdmin)
+admin.site.register(Major, MajorAdmin)
--- collegespark/core/models.py
+++ collegespark/core/models.py
@@ -1,16 +1,71 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
-class User(AbstractUser):
-    # school = models.ForeignKey('School')
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, school, password=None):
+        user = self.model(email=UserManager.normalize_email(email), username=username, school=school)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        print School.objects.all()
+        school = School.objects.filter(id=1)[0]
+        user = self.create_user(email, password=password, username=username, school=school)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True, max_length=255, blank=False, db_index=True)
+    username = models.CharField(max_length=30, blank=False, unique=True)
     major = models.ForeignKey('Major', blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    school = models.ForeignKey('School')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+    def __repr__(self):
+        return "<User {}>".format(username)
 
 
 class School(models.Model):
-    pass
+    name = models.CharField(max_length=40)
+
+    def __repr__(self):
+        return "<School '{}'>".format(self.name)
 
 
 class Major(models.Model):
-    pass
+    name = models.CharField(max_length=40)
