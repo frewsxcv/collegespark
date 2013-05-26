e46da3053fae89e0b9b429af4cddbd0634cbb47e
Corey Farwell <coreyf@rwell.org>
2013-05-26 12:55:08 -0700

    add school name to chat title

--- collegespark/chat/views.py
+++ collegespark/chat/views.py
@@ -4,4 +4,5 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/')
 def chat(request, school_name):
-    return render(request, 'chat/chat.html')
+    data = {'school_name': school_name}
+    return render(request, 'chat/chat.html', data)
