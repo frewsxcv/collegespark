1dd68fcccb250b568a5b7fdaf228ce0469e3f359
ziyang <ziyang0621@gmail.com>
2013-05-30 23:47:32 -0700

    change settings temp

--- collegespark/settings.template.py
+++ collegespark/settings.template.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hormozsucks.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': 'mydb.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -52,7 +52,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = PWD + "/media"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -63,7 +63,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), 'static/media/'))
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -133,8 +133,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'collegespark.core',
     'collegespark.book',
-    'collegespark.chat',
-    'collegespark.discussion',
+    'collegespark.discussion'
 )
 
 # A sample logging configuration. The only tangible logging
