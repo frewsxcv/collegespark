cbc7d81a113c312cd3cc2ec10d01a36ad4bb137d
Corey Farwell <coreyf@rwell.org>
2013-05-30 20:30:49 -0700

    small changes

--- collegespark/static/js/chat/chat.js
+++ collegespark/static/js/chat/chat.js
@@ -5,22 +5,24 @@ $(function () {
     var Message = function (obj) {
         this.time = moment(obj.time).format('h:mm');
         this.text = obj.msg;
+        this.user = obj.user;
     };
 
     Message.prototype.html = function () {
         var html = "<li class='chat-msg'>" +
             "<span class='label label-inverse'>" + this.time + "</span>&nbsp;" +
-            "<span class='label'>Corey</span>: " + 
+            "<span class='label label-info'>" + this.user + "</span>: " + 
             this.text + "</li>";
         return $(html);
     };
 
 
     var Chat = function () {
+        this.room = defaultRoom;
+        this.user = user;
         this._cache();
         this._events();
         this.connect();
-        this.room = defaultRoom;
     };
 
     Chat.prototype._cache = function () {
@@ -44,12 +46,21 @@ $(function () {
     Chat.prototype.connect = function () {
         var that = this;
         this.socket = io.connect('http://' + window.location.hostname + ':8001');
+        this.socket.on('history', function (data) {
+            data.history.forEach(function (m) {
+                var msg = new Message(m),
+                    html = msg.html();
+                that.$msgs.append(html);
+            });
+            that.$msgs.children().last().get(0).scrollIntoView();
+        });
         this.socket.on('broadcast', function (data) {
             var msg = new Message(data),
                 html = msg.html();
             that.$msgs.append(html);
             html.get(0).scrollIntoView();
         });
+        this.socket.emit('history', {'room': this.room});
     };
 
     Chat.prototype.send = function () {
@@ -57,7 +68,8 @@ $(function () {
         if (msg.length > 0) {
             this.socket.emit('send', {
                 'room': this.room,
-                'msg': msg
+                'msg': msg,
+                'user': this.user
             });
             this.$inputText.val("");
         } else {
